578a9a5bdc145e01a0212fd913205ffde255f158
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-04 19:38:32 +0300

    feat: added user model

---
+++ src/main/java/com/gymohrim/entity/User.java
@@ -0,0 +1,34 @@
+package com.gymohrim.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+}
