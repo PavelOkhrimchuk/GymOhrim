3a92e40883a1477390411752df9953e51fe44bac
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-04 19:38:50 +0300

    feat: added user profile model

---
+++ src/main/java/com/gymohrim/entity/UserProfile.java
@@ -0,0 +1,41 @@
+package com.gymohrim.entity;
+
+
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user_profile")
+
+public class UserProfile {
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Double weight;
+
+    private Double height;
+
+    private String gender;
+
+    @Column(name = "profile_picture_url")
+    private String profilePictureUrl;
+
+    private Date birthDate;
+}
