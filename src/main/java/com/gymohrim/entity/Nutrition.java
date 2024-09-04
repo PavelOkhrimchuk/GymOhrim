a77f17bb191773d0f151ab2429c98719a1a7d822
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-04 19:38:15 +0300

    feat: added nutrition model

---
+++ src/main/java/com/gymohrim/entity/Nutrition.java
@@ -0,0 +1,38 @@
+package com.gymohrim.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "nutrition")
+public class Nutrition {
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    private Integer calories;
+    private Double protein;
+    private Double fat;
+    private Double carbohydrates;
+
+}
