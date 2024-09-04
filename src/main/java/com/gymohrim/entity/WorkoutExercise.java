7d429d96a2af3b40ef2f1c94299ab6a1e48f7701
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-04 19:39:18 +0300

    feat: added workout exercise model

---
+++ src/main/java/com/gymohrim/entity/WorkoutExercise.java
@@ -0,0 +1,38 @@
+package com.gymohrim.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "workout_exercise")
+
+public class WorkoutExercise {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id", nullable = false)
+    private Workout workout;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+
+    private Integer sets;
+    private Integer reps;
+    private Double weight;
+
+
+}
