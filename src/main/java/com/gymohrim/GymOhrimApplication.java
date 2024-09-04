91c3d0463be0286cc79d2042968b0de9d7c35c2c
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-04 14:00:16 +0300

    Initial commit

---
+++ src/main/java/com/gymohrim/GymOhrimApplication.java
@@ -0,0 +1,13 @@
+package com.gymohrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymOhrimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymOhrimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gymohrim/GymOhrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymohrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymOhrimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
