27a2f9473aad88f81017d223626b9739d033c8af
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-09-09 23:14:31 +0300

    feat: csrf protection

--- src/main/java/com/gymohrim/configuration/SecurityConfiguration.java
+++ src/main/java/com/gymohrim/configuration/SecurityConfiguration.java
@@ -2,19 +2,17 @@ package com.gymohrim.configuration;
 
 import com.gymohrim.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +27,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/register", "/login").permitAll()
                         .anyRequest().authenticated()
@@ -39,9 +37,7 @@ public class SecurityConfiguration {
                         .defaultSuccessUrl("/profile", true)
                         .permitAll()
                 )
-                .logout(logout -> logout
-                        .permitAll()
-                )
+                .logout(LogoutConfigurer::permitAll)
                 .build();
     }
 
