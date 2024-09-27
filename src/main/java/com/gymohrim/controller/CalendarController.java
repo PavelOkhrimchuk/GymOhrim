0d894235947981ab02ebc9c4ddcb7e841c1b31ef
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-09-27 20:47:27 +0300

    feat: save daily record first implementation

--- src/main/java/com/gymohrim/controller/CalendarController.java
+++ src/main/java/com/gymohrim/controller/CalendarController.java
@@ -1,6 +1,12 @@
 package com.gymohrim.controller;
 
 
+import com.gymohrim.entity.User;
+import com.gymohrim.service.statistics.DailyRecordService;
+import com.gymohrim.service.user.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/calendar")
 public class CalendarController {
+    private final UserProfileService userProfileService;
+    private final DailyRecordService dailyRecordService;
 
     @GetMapping
     public String showCalendar() {
@@ -19,10 +32,22 @@ public class CalendarController {
 
 
     @PostMapping("/submit")
-    public String submitDate(@RequestParam("selectedDate") String selectedDate, Model model) {
-        model.addAttribute("selectedDate", selectedDate);
+    public String submitDate(@RequestParam("selectedDate") String selectedDate,
+                             @AuthenticationPrincipal UserDetails userDetails, Model model) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = dateFormat.parse(selectedDate);
 
-        return "redirect:/profile";
+            User user = userProfileService.findByEmail(userDetails.getUsername());
+            dailyRecordService.saveDailyRecord(date, user);
 
+            return "redirect:/profile";
+
+        } catch (ParseException e) {
+            model.addAttribute("error", "Invalid date format");
+            return "calendar-form";
+
+
+        }
     }
 }
--- src/main/java/com/gymohrim/service/statistics/DailyRecordService.java
+++ src/main/java/com/gymohrim/service/statistics/DailyRecordService.java
@@ -1,14 +1,29 @@
 package com.gymohrim.service.statistics;
 
 
+import com.gymohrim.entity.DailyRecord;
+import com.gymohrim.entity.User;
 import com.gymohrim.repository.DailyRecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class DailyRecordService {
     private  final DailyRecordRepository dailyRecordRepository;
+
+    public void saveDailyRecord(Date date, User user) {
+        DailyRecord dailyRecord = DailyRecord.builder()
+                .user(user)
+                .date(date)
+                .build();
+
+        dailyRecordRepository.save(dailyRecord);
+    }
+
+
 
     
 }
