abc71c7eea76dd7f052e941e5c057f476040cc0e
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-09-28 19:29:55 +0300

    feat: added methods to nutrition and workout repository

--- src/main/java/com/gymohrim/repository/NutritionRepository.java
+++ src/main/java/com/gymohrim/repository/NutritionRepository.java
@@ -1,10 +1,14 @@
 package com.gymohrim.repository;
 
+import com.gymohrim.entity.DailyRecord;
 import com.gymohrim.entity.Nutrition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface NutritionRepository extends JpaRepository<Nutrition, Integer> {
+    Optional<Nutrition> findByDailyRecord(DailyRecord dailyRecord);
 }
--- src/main/java/com/gymohrim/repository/WorkoutRepository.java
+++ src/main/java/com/gymohrim/repository/WorkoutRepository.java
@@ -1,10 +1,14 @@
 package com.gymohrim.repository;
 
+import com.gymohrim.entity.DailyRecord;
 import com.gymohrim.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout,Integer> {
+    Optional<Workout> findByDailyRecord(DailyRecord dailyRecord);
 }
