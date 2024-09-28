84b17c95e12311b5f74bcb3e1ede7fd94f4a157f
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-09-28 19:29:38 +0300

    feat: added methods to nutrition and workout service

--- src/main/java/com/gymohrim/service/statistics/NutritionService.java
+++ src/main/java/com/gymohrim/service/statistics/NutritionService.java
@@ -1,13 +1,34 @@
 package com.gymohrim.service.statistics;
 
 
+import com.gymohrim.entity.Nutrition;
 import com.gymohrim.repository.NutritionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class NutritionService {
 
     private final NutritionRepository nutritionRepository;
+
+    public void saveOrUpdateNutrition(Nutrition nutrition) {
+
+
+        Optional<Nutrition> existingNutrition = nutritionRepository.findByDailyRecord(nutrition.getDailyRecord());
+        if(existingNutrition.isPresent()) {
+            Nutrition existing = existingNutrition.get();
+
+            existing.setCalories(nutrition.getCalories());
+            existing.setProtein(nutrition.getProtein());
+            existing.setFat(nutrition.getFat());
+            existing.setCarbohydrates(nutrition.getCarbohydrates());
+            nutritionRepository.save(existing);
+        }
+        else {
+            nutritionRepository.save(nutrition);
+        }
+    }
 }
