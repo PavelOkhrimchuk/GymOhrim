027270e17b38933b9a7970340cc99b21884c47ed
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-09-30 22:17:11 +0300

    feat: added Nutriments dto

---
+++ src/main/java/com/gymohrim/dto/openfoodfacts/api/Nutriments.java
@@ -0,0 +1,25 @@
+package com.gymohrim.dto.openfoodfacts.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Nutriments {
+
+    @JsonProperty("proteins")
+    private double proteins;
+
+    @JsonProperty("fat")
+    private double fat;
+
+    @JsonProperty("carbohydrates")
+    private double carbohydrates;
+
+    @JsonProperty("energy-kcal")
+    private double energyKcal;
+}
