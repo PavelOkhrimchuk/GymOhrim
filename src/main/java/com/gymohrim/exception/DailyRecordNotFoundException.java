ddbd0ce10b34b5d62942ef5b7652f84ad83937cf
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-11 03:00:18 +0300

    feat: DailyRecordNotFoundException

---
+++ src/main/java/com/gymohrim/exception/DailyRecordNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gymohrim.exception;
+
+public class DailyRecordNotFoundException extends RuntimeException{
+
+    public DailyRecordNotFoundException(String message) {
+        super(message);
+    }
+}
