78692435acd5106710f29aeaabd3e6df8bfadd10
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-14 00:22:03 +0300

    feat: OAuth2 integration in the OAuth2UserServiceImpl

--- src/main/java/com/gymohrim/service/user/OAuth2UserServiceImpl.java
+++ src/main/java/com/gymohrim/service/user/OAuth2UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.gymohrim.service.user;
 
+import com.gymohrim.configuration.SecurityConfiguration;
+import com.gymohrim.entity.User;
+import com.gymohrim.entity.account.AuthProviderType;
+import com.gymohrim.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -8,6 +12,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +21,32 @@ public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserReques
 
 
 
+    private final UserRepository userRepository;
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
 
-        return oAuth2User;
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        User user = userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setName(name);
+            String randomPassword = UUID.randomUUID().toString();
+            newUser.setPassword(SecurityConfiguration.encodePassword(randomPassword));
+            newUser.setAuthProviderType(AuthProviderType.GOOGLE);
+            return userRepository.save(newUser);
+        });
+
+        return new CustomOAuth2User(oAuth2User, user.getPassword());
     }
+
+
+
+
+
 }
 
