31f3f4a28f09e3d6b613957b0e50d8555c28ae43
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-14 00:21:00 +0300

    feat: CustomOAuth2User first implementation

---
+++ src/main/java/com/gymohrim/service/user/CustomOAuth2User.java
@@ -0,0 +1,65 @@
+package com.gymohrim.service.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User, UserDetails {
+
+
+    private OAuth2User oAuth2User;
+    private String password;
+
+    public CustomOAuth2User(OAuth2User oAuth2User, String password) {
+        this.oAuth2User = oAuth2User;
+        this.password = password;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2User.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return oAuth2User.getAuthorities();
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2User.getAttribute("name");
+    }
+
+    @Override
+    public String getUsername() {
+        return oAuth2User.getAttribute("email");
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
