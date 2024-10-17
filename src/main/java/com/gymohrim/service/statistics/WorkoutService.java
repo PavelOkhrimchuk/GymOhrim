9f2e7620818e019c6a82f60a57d28c2a4e3dfe60
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-17 18:53:40 +0300

    feat: adding logging to the WorkoutService

--- src/main/java/com/gymohrim/service/statistics/WorkoutService.java
+++ src/main/java/com/gymohrim/service/statistics/WorkoutService.java
@@ -4,12 +4,14 @@ package com.gymohrim.service.statistics;
 import com.gymohrim.entity.Workout;
 import com.gymohrim.repository.WorkoutRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WorkoutService {
 
     private final WorkoutRepository workoutRepository;
@@ -23,13 +25,16 @@ public class WorkoutService {
             existing.setWorkoutType(workout.getWorkoutType());
             existing.setNotes(workout.getNotes());
             workoutRepository.save(existing);
+            log.info("Updated Workout ID: {}", existing.getId());
         } else {
 
             workoutRepository.save(workout);
+            log.info("Saved new Workout ID: {}", workout.getId());
         }
     }
 
     public Workout findById(Integer id) {
+        log.info("Finding Workout with ID: {}", id);
         return workoutRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Workout with id " + id + " not found"));
     }
