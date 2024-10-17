f260f2a86403998d97cf2f74218b85f3883ad673
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-17 18:54:32 +0300

    feat: adding logging to the OpenFoodFactsService

--- src/main/java/com/gymohrim/service/api/OpenFoodFactsService.java
+++ src/main/java/com/gymohrim/service/api/OpenFoodFactsService.java
@@ -2,10 +2,12 @@ package com.gymohrim.service.api;
 
 import com.gymohrim.dto.openfoodfacts.api.ProductDetailsDto;
 import com.gymohrim.dto.openfoodfacts.api.ProductResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class OpenFoodFactsService {
 
     private final RestTemplate restTemplate;
@@ -15,6 +17,7 @@ public class OpenFoodFactsService {
     }
 
     public ProductDetailsDto getProductInfo(String barcode) {
+        log.info("Fetching product information for barcode: {}", barcode);
         String url = "https://world.openfoodfacts.net/api/v2/product/" + barcode;
         ProductResponseDto response = restTemplate.getForObject(url, ProductResponseDto.class);
         return response != null ? response.getProduct() : null;
