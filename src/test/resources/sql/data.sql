7af224bac331132b903106fc6d38e54bb1add8d7
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-20 22:40:35 +0300

    feat: basic data.sql for tests

---
+++ src/test/resources/sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, name, password) VALUES
+                                              ('test1@example.com', 'Test User 1', 'password1'),
+                                              ('test2@example.com', 'Test User 2', 'password2'),
+                                              ('test3@example.com', 'Test User 3', 'password3');
