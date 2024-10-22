aebdcc905418d13d97e14d62dca264276138dceb
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-10-22 20:50:49 +0300

    configuration: refactor configuration for tests

--- src/test/java/com/gymohrim/IntegrationTestBase.java
+++ src/test/java/com/gymohrim/IntegrationTestBase.java
@@ -1,32 +1,31 @@
 package com.gymohrim;
 
-import com.gymohrim.annotation.IT;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@IT
 @Sql({
         "classpath:sql/data.sql"
 })
+
+@Testcontainers
 public abstract class IntegrationTestBase {
-    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:15.3");
+    private static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:15.3")
+                    .withDatabaseName("test")
+                    .withUsername("test")
+                    .withPassword("test");
 
-
-    @BeforeAll
-    static void setUp() {
+    static {
         container.start();
     }
 
-
-
     @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
+    static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.datasource.username", container::getUsername);
         registry.add("spring.datasource.password", container::getPassword);
-        registry.add("spring.datasource.driver-class-name", container::getDriverClassName);
     }
 }
