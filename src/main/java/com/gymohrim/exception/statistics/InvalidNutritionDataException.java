f94d3b4aec07533c2f11b20524cffc0c6d96a718
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-18 23:27:01 +0300

    feat: InvalidNutritionDataException exception implementation

---
+++ src/main/java/com/gymohrim/exception/statistics/InvalidNutritionDataException.java
@@ -0,0 +1,7 @@
+package com.gymohrim.exception.statistics;
+
+public class InvalidNutritionDataException extends RuntimeException{
+    public InvalidNutritionDataException(String message) {
+        super(message);
+    }
+}
