bf8f70c9144a22f9488b2717f3e6e53ee1392bef
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-18 23:11:35 +0300

    feat: ExerciseNotFound exception implementation

---
+++ src/main/java/com/gymohrim/exception/statistics/ExerciseNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gymohrim.exception.statistics;
+
+public class ExerciseNotFoundException extends RuntimeException{
+
+    public ExerciseNotFoundException(String message) {
+        super(message);
+    }
+}
