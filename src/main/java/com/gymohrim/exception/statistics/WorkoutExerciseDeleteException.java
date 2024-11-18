960003f5eb9b2caa1b9871152d62127ad6a9986a
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-18 23:17:55 +0300

    feat: WorkoutExerciseDeleteException exception implementation

---
+++ src/main/java/com/gymohrim/exception/statistics/WorkoutExerciseDeleteException.java
@@ -0,0 +1,7 @@
+package com.gymohrim.exception.statistics;
+
+public class WorkoutExerciseDeleteException extends RuntimeException {
+    public WorkoutExerciseDeleteException(String message) {
+        super(message);
+    }
+}
